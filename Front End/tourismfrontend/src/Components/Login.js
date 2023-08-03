0c308c5060f5ccac9842e7a1910367605479f91f
Ajay Gokul <ajaygokulpm@gmail.com>
2023-08-03 19:00:57 +0530

    Micro FE

---
+++ Front End/tourismfrontend/src/Components/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Login.css';
+import { MDBRow } from 'mdb-react-ui-kit'; 
+
+function Login()
+{
+    return(
+       <div>
+          <div className="ImageDiv">
+              <img src=""/>
+          </div>
+       </div>
+    );
+}
+
+export default Login;
